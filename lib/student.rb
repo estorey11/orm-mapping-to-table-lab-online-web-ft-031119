555ea3b11144e0b613174721de7a38969c5c6dca
Eric Storey <estorey@gmail.com>
2019-04-14 19:39:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,4 +23,16 @@ class Student
     DB[:conn].execute(sql) 
   end
   
+  def self.drop_table
+    sql= <<-SQL 
+    DROP TABLE students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER
+    );
+    SQL
+    
+    DB[:conn].execute(sql) 
+  end
+  
 end
